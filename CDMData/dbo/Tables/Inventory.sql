d3d6b997ea197f6e46dcf923f2f276257595d49d
Antonio Lopes Da Cruz <tonycruz@outlook.com>
2020-10-11 19:04:27 +0100

    Register DB Tables

---
+++ CDMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductId] int not null,
+	[Quantity] int not null,
+	[PurchasePrice] money not null,
+	[PurchaseDate] Datetime2 not null
+)
---
+++ CDMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[SaleId] INT NOT NULL,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL,
+	[PurchasePrice] Money not null,
+	[Tax] Money not null default 0
+)
